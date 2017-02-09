5691063a6a3b2cd60f8cac27ebfdc591ee807f22
Timothee Goguely <timotheegoguely@gmail.com>
2017-02-09 18:21:38 +0100

    add image to cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,22 @@
 require 'open-uri'
 require 'json'
 
+# Ingredients
 url_ingredients = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 ingredients = JSON.parse(open(url_ingredients).read)
 
-# Ingredient.destroy_all
+Ingredient.destroy_all
 
-# ingredients["drinks"].each do |ingredient|
-#   Ingredient.create!(name: ingredient.values.to_s)
-# end
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient.values.to_s)
+end
 
-
+# Cocktails
 url_cocktails = "http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail"
 cocktails = JSON.parse(open(url_cocktails).read)
 
-# Cocktail.destroy_all
+Cocktail.destroy_all
 
 cocktails["drinks"].each do |cocktail|
-  Cocktail.create!(name: cocktail["strDrink"].to_s)
+  Cocktail.create!(name: cocktail["strDrink"].to_s, image: cocktail["strDrinkThumb"].to_s)
 end
